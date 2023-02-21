6543949ea911dff50229e91897eb7a7999963e93
Halimeh <101599560+Lahali@users.noreply.github.com>
2023-02-21 14:54:42 +0100

    added navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react"
+import { NavLink, useLocation, useNavigate } from "react-router-dom"
+// import { IconArrow, IconInfo } from "./atoms"
+
+function Navbar({ children }) {
+  //   const [showInfo, setShowInfo] = useState(true)
+  //   const location = useLocation()
+
+  //   useEffect(() => {
+  //     if (location.pathname === "/information") {
+  //       setShowInfo(false)
+  //     }
+  //   }, [location])
+
+  // Función para navegar a la página anterior
+  const navigate = useNavigate()
+  const handleBack = () => {
+    navigate(-1)
+  }
+
+  return (
+    <div className="fixed z-[1000] flex w-full items-center justify-between rounded-b-xl bg-black p-3 text-center text-white">
+      <button onClick={handleBack}></button>
+      <div className="flex flex-grow justify-center">{children}</div>
+      {/* {showInfo && (
+        <NavLink
+          to="/information"
+          className={`${
+            location.pathname === "/information" ? "text-primary" : ""
+          }`}
+        ></NavLink>
+      )} */}
+    </div>
+  )
+}
+
+export default Navbar
