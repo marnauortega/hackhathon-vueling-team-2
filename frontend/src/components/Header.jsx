4cbc2020f1631883e4b2a1d37b03c3672e51c1bb
An_34 <anna.villar34@gmail.com>
2023-02-21 14:46:46 +0100

    finished form styling

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -14,6 +14,9 @@ const Header = () => {
           <li>
             <Link to="/login">Login</Link>
           </li>
+          <li>
+            <Link to="/formPage">Form</Link>
+          </li>
         </ul>
       </nav>
     </header>
