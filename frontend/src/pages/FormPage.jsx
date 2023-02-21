86ef815bb67035b2ca335405c2e47090697d82e1
An_34 <anna.villar34@gmail.com>
2023-02-21 15:42:18 +0100

    controlled inputs

--- frontend/src/pages/FormPage.jsx
+++ frontend/src/pages/FormPage.jsx
@@ -1,7 +1,21 @@
 import React, { useState } from "react";
 
 export const FormPage = () => {
-  const [data, setdata] = useState({});
+  // Full Time states
+  const [fullTimeJardinera, setFullTimeJardinera] = useState(6);
+  const [fullTimeEquipaje, setFullTimeEquipaje] = useState(7.25);
+  const [fullTimeCoordinacion, setFullTimeCoordinacion] = useState(10);
+
+  // Part Time states
+
+  const [partTimeJardinera, setPartTimeJardinera] = useState(7.5);
+  const [partTimeEquipaje, setpartTimeEquipaje] = useState(7);
+  const [partTimeCoordinacion, setPartTimeCoordinacion] = useState(8.5);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <div className="flex align-center h-full justify-center px-4 text-[#4d4d4d]">
@@ -11,10 +25,9 @@ export const FormPage = () => {
               <h1 className="flex text-gray-700 justify-center items-center font-semibold text-2xl">
                 Edit logistic data
               </h1>
-
               {/* JARDINERA */}
               <h3 className="flex text-gray-700 justify-center items-center font-semibold">
-                Jardinera
+                Jardinera {`${'€/h'}`}
               </h3>
               <div className="flex gap-6">
                 <div className="">
@@ -27,10 +40,11 @@ export const FormPage = () => {
                   <div className="mt-1 flex rounded-md shadow-sm text-gray-700 ">
                     <input
                       type="number"
+                      step='any'
                       name="fullTimeJardinera"
                       min={0}
-                      // value={data.date}
-                      // onChange={(e) => handleInputChange(e)}
+                      onChange={(e) => setFullTimeJardinera(e.target.value)}
+                      value={fullTimeJardinera}
                       className="block w-full flex-1  sm:text-sm bg-white p-1 rounded-sm text-gray-700"
                       placeholder="€/h"
                     />
@@ -46,10 +60,11 @@ export const FormPage = () => {
                   <div className="mt-1 flex rounded-md shadow-sm text-gray-700 ">
                     <input
                       type="number"
+                      step='any'
                       name="partTimeJardinera"
                       min={0}
-                      // value={data.date}
-                      // onChange={(e) => handleInputChange(e)}
+                      onChange={(e) => setPartTimeJardinera(e.target.value)}
+                      value={partTimeJardinera}
                       className="block w-full flex-1  sm:text-sm bg-white p-1 rounded-sm text-gray-700"
                       placeholder="€/h"
                     />
@@ -59,7 +74,7 @@ export const FormPage = () => {
 
               {/* EQUIPAJE */}
               <h3 className="flex text-gray-700 justify-center items-center font-semibold">
-                Equipaje
+                Equipaje {`${'€/h'}`}
               </h3>
               <div className="flex gap-6">
                 <div className="">
@@ -72,10 +87,11 @@ export const FormPage = () => {
                   <div className="mt-1 flex rounded-md shadow-sm text-gray-700 ">
                     <input
                       type="number"
+                      step='any'
                       name="fullTimeEquipaje"
                       min={0}
-                      // value={data.date}
-                      // onChange={(e) => handleInputChange(e)}
+                      onChange={(e) => setFullTimeEquipaje(e.target.value)}
+                      value={fullTimeEquipaje}
                       className="block w-full flex-1  sm:text-sm bg-white p-1 rounded-sm text-gray-700"
                       placeholder="€/h"
                     />
@@ -91,10 +107,11 @@ export const FormPage = () => {
                   <div className="mt-1 flex rounded-md shadow-sm text-gray-700 ">
                     <input
                       type="number"
+                      step='any'
                       name="partTimeEquipaje"
                       min={0}
-                      // value={data.date}
-                      // onChange={(e) => handleInputChange(e)}
+                      onChange={(e) => setpartTimeEquipaje(e.target.value)}
+                      value={partTimeEquipaje}
                       className="block w-full flex-1  sm:text-sm bg-white p-1 rounded-sm text-gray-700"
                       placeholder="€/h"
                     />
@@ -103,7 +120,7 @@ export const FormPage = () => {
               </div>
               {/* Coordinacion */}
               <h3 className="flex text-gray-700 justify-center items-center font-semibold">
-                Coordinación
+                Coordinación {`${'€/h'}`}
               </h3>
               <div className="flex gap-6">
                 <div className="">
@@ -116,10 +133,11 @@ export const FormPage = () => {
                   <div className="mt-1 flex rounded-md shadow-sm text-gray-700 ">
                     <input
                       type="number"
+                      step='any'
                       name="fullTimeCoordinacion"
                       min={0}
-                      // value={data.date}
-                      // onChange={(e) => handleInputChange(e)}
+                      onChange={(e) => setFullTimeCoordinacion(e.target.value)}
+                      value={fullTimeCoordinacion}
                       className="block w-full flex-1  sm:text-sm bg-white p-1 rounded-sm text-gray-700"
                       placeholder="€/h"
                     />
@@ -135,10 +153,11 @@ export const FormPage = () => {
                   <div className="mt-1 flex rounded-md shadow-sm text-gray-700 ">
                     <input
                       type="number"
+                      step='any'
                       name="partTimeCoordinacion"
                       min={0}
-                      // value={data.date}
-                      // onChange={(e) => handleInputChange(e)}
+                      onChange={(e) => setPartTimeCoordinacion(e)}
+                      value={partTimeCoordinacion}
                       className="block w-full flex-1  sm:text-sm bg-white p-1 rounded-sm text-gray-700"
                       placeholder="€/h"
                     />
@@ -149,7 +168,7 @@ export const FormPage = () => {
               {/* button */}
               <div className="px-4 py-3 text-right sm:px-6">
                 <button
-                  type="submit"
+                  onClick={(e) => handleFormSubmit}
                   className="inline-flex justify-center items-center text-sm font-medium   hover:bg-slate-600 rounded-full bg-[#4d4d4d] text-white px-8 py-2"
                 >
                   Send
